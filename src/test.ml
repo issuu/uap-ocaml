e96d66944a949f61eb6b0143667be8fe986c015c
Anders Hellerup Madsen <anders@hellerup-madsen.dk>
2018-10-04 21:01:21 +0200

    Initial commit

--- src/test.ml
+++ src/test.ml
@@ -1,11 +1,98 @@
-let test_parser () =
-    Alcotest.(check int) "Should parse" 42 User_agent_parser.hest
+open Base
+open User_agent_parser
+
+let ua_result = Alcotest.testable UAParser.pp_result UAParser.equal_result
+let os_result = Alcotest.testable OSParser.pp_result OSParser.equal_result
+let device_result = Alcotest.testable DeviceParser.pp_result DeviceParser.equal_result
+
+let load_file : string -> string = fun filename ->
+  let channel = Stdio.In_channel.create filename in
+  let len = Stdio.In_channel.length channel |> Int64.to_int_exn in
+  let buf = Bytes.create len in
+  Stdio.In_channel.really_input_exn channel ~buf ~len ~pos:0;
+  Stdio.In_channel.close channel;
+  Caml.Printf.sprintf "%s" @@ Bytes.to_string buf
+
+let quick_test_ua_parser () =
+  let t = UAParser.init () in
+  let expected = UAParser.{ family = "Firefox (Minefield)"; major = Some "4"; minor = Some "0"; patch = Some "1pre"; } in
+  let actual = UAParser.parse t "Mozilla/5.0 (Windows; Windows NT 5.1; rv:2.0b3pre) Gecko/20100727 Minefield/4.0.1pre" in
+  Alcotest.(check ua_result) "corectly parsed browser family" expected actual
+
+let quick_test_os_parser () =
+  let t = OSParser.init () in
+  let expected = OSParser.{ family = "Windows"; major = Some "95"; minor = None; patch = None; patch_minor = None } in
+  let actual = OSParser.parse t "Mozilla/5.0 (Windows; U; Win95; en-US; rv:1.1) Gecko/20020826" in
+  Alcotest.(check os_result) "corretly parsed os" expected actual
+
+let quick_test_device_parser () =
+  let t = DeviceParser.init () in
+  let expected = DeviceParser.{ family = "Odys PEDI PLUS W"; brand = Some "Odys"; model = Some "PEDI PLUS W" } in
+  let actual = DeviceParser.parse t "Mozilla/5.0 (Linux; U; Android 4.2.2; de-de; PEDI_PLUS_W Build/JDQ39) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Safari/534.30" in
+  Alcotest.(check device_result) "correctly parsed device" expected actual
+
+let yaml_to_test_cases_assoc s =
+  Yaml.of_string s
+  |> Result.map_error ~f:(function `Msg s -> s)
+  |> Result.ok_or_failwith
+  |> function
+    | `O [("test_cases", `A seq)] -> List.map seq ~f:(function
+      | `O assoc -> List.Assoc.map assoc ~f:(function 
+          | `String v -> v
+          | _ -> failwith "bad test case yaml")
+      | _ -> failwith "bad test case yaml")
+    | _ -> failwith "bad test case yaml"
+
+let test_ua_parser () =
+  let t = UAParser.init () in
+  List.map (yaml_to_test_cases_assoc (load_file "../uap-core/tests/test_ua.yaml")) ~f:(fun assoc ->
+    let user_agent_string = List.Assoc.find_exn assoc ~equal:String.equal "user_agent_string" in
+    let family = List.Assoc.find_exn assoc ~equal:String.equal "family" in
+    let major = List.Assoc.find_exn assoc ~equal:String.equal "major" |> function "" -> None | s -> Some s in
+    let minor = List.Assoc.find_exn assoc ~equal:String.equal "minor" |> function "" -> None | s -> Some s in
+    let patch = List.Assoc.find_exn assoc ~equal:String.equal "patch" |> function "" -> None | s -> Some s in
+    Alcotest.test_case user_agent_string `Slow (fun () ->
+      let expected = UAParser.{ family ; major; minor; patch } in
+      let actual = UAParser.parse t user_agent_string in
+      Alcotest.(check ua_result) user_agent_string expected actual))
+
+let test_os_parser () =
+  let t = OSParser.init () in
+  List.map (yaml_to_test_cases_assoc (load_file "../uap-core/tests/test_os.yaml")) ~f:(fun assoc ->
+    let user_agent_string = List.Assoc.find_exn assoc ~equal:String.equal "user_agent_string" in
+    let family = List.Assoc.find_exn assoc ~equal:String.equal "family" in
+    let major = List.Assoc.find_exn assoc ~equal:String.equal "major" |> function "" -> None | s -> Some s in
+    let minor = List.Assoc.find_exn assoc ~equal:String.equal "minor" |> function "" -> None | s -> Some s in
+    let patch = List.Assoc.find_exn assoc ~equal:String.equal "patch" |> function "" -> None | s -> Some s in
+    let patch_minor = List.Assoc.find_exn assoc ~equal:String.equal "patch_minor" |> function "" -> None | s -> Some s in
+    Alcotest.test_case user_agent_string `Slow (fun () ->
+      let expected = OSParser.{ family ; major; minor; patch; patch_minor } in
+      let actual = OSParser.parse t user_agent_string in
+      Alcotest.(check os_result) "test" expected actual))
+
+let test_device_parser () =
+  let t = DeviceParser.init () in
+  List.map (yaml_to_test_cases_assoc (load_file "../uap-core/tests/test_device.yaml")) ~f:(fun assoc ->
+    let user_agent_string = List.Assoc.find_exn assoc ~equal:String.equal "user_agent_string" in
+    let family = List.Assoc.find_exn assoc ~equal:String.equal "family" in
+    let brand = List.Assoc.find_exn assoc ~equal:String.equal "brand" |> function "" -> None | s -> Some s in
+    let model = List.Assoc.find_exn assoc ~equal:String.equal "model" |> function "" -> None | s -> Some s in
+    Alcotest.test_case user_agent_string `Slow (fun () ->
+      let expected = DeviceParser.{ family; brand; model } in
+      let actual = DeviceParser.parse t user_agent_string in
+      Alcotest.(check device_result) (Caml.Printf.sprintf "'%s'" user_agent_string) expected actual))
+
 
 let set = [
-    Alcotest.test_case "Test parser" `Quick test_parser;
+    Alcotest.test_case "Test useragent parser" `Quick quick_test_ua_parser;
+    Alcotest.test_case "Test os parser" `Quick quick_test_os_parser;
+    Alcotest.test_case "Test device parser" `Quick quick_test_device_parser;
 ]
 
 let () =
   Alcotest.run "test suite" [
     ("test", set);
+    ("ua_parser", test_ua_parser ());
+    ("os_parser", test_os_parser ());
+    ("device_parser", test_device_parser ());
   ]
--- src/user_agent_parser.ml
+++ src/user_agent_parser.ml
@@ -1,5 +1,163 @@
-let hest = 42
+open Base
+module Format = Caml.Format
 
-module Test = struct
+module Parser = struct
+  module Rule = struct
+    type t = {
+      regex: Re.re;
+      replacements: (string * string) list;
+      pattern: string;
+    } [@@deriving show]
+
+    let of_yaml: Yaml.value -> t = function
+      | `O assoc -> 
+        let pattern =
+          List.Assoc.find_exn assoc ~equal:String.equal "regex"
+          |> (function `String s -> s | _ -> failwith "invalid yaml")
+        in
+        let flags =
+          List.Assoc.find assoc ~equal:String.equal "regex_flag"
+          |> function Some `String "i" -> [`CASELESS] | _ -> []
+        in
+        let regex = Re.Pcre.regexp ~flags pattern in
+        let replacements = List.fold_right assoc ~init:[] ~f:(fun o acc ->
+          match o with
+          | ("regex", _) -> acc 
+          | ("regex_flag", _) -> acc 
+          | (k, `String v) -> (k, v) :: acc
+          | _ -> failwith "invalid yaml")
+        in
+        { regex; replacements; pattern }
+      | _ -> failwith "invalid yaml"
+
+    let apply t s =
+      match Re.exec_opt t.regex s with
+      | Some groups -> Some groups
+      | None -> None
+  end
+
+  module Result = struct
+    type groups = string list [@@deriving show, eq]
+    type replacers = (string * string) list [@@deriving show, eq]
+    type t = groups * replacers [@@deriving show, eq]
+
+    let replace_regex = Re.Pcre.regexp "\\$\\d+"
+
+    let replace s matches =
+      Re.replace replace_regex s ~all:true ~f:(fun m ->
+        Option.value ~default:"" @@ match (Re.Group.get m 0) with
+        | "$1" -> List.nth matches 1
+        | "$2" -> List.nth matches 2
+        | "$3" -> List.nth matches 3
+        | "$4" -> List.nth matches 4
+        | "$5" -> List.nth matches 5
+        | "$6" -> List.nth matches 6
+        | "$7" -> List.nth matches 7
+        | "$8" -> List.nth matches 8
+        | "$9" -> List.nth matches 9
+        | _ -> None)
+
+    let get (groups, replacers) (s, idx) =
+      let r = match List.Assoc.find replacers  ~equal:String.equal s with
+        | Some rep -> Some (replace rep groups)
+        | None -> List.nth groups idx
+      in
+      Option.map r ~f:String.strip |> function Some "" -> None | x -> x
+  end
+
+  type t = Rule.t list [@@deriving show]
+
+  let of_yaml: Yaml.value -> t = function
+    | `A seq -> List.map seq ~f:Rule.of_yaml
+    | _ -> failwith "invalid yaml"
+  
+  let apply: t -> string -> Result.t = fun t s ->
+    List.find_map t ~f:(fun rule ->
+      match Rule.apply rule s with
+      | Some groups -> Some (Re.Group.all groups |> Array.to_list, rule.replacements)
+      | None -> None)
+    |> Option.value ~default:([ "Other" ], [])
 end
 
+
+module UAParser = struct
+  type t = Parser.t
+
+  type result = {
+    family: string;
+    major: string option;
+    minor: string option;
+    patch: string option;
+  } [@@deriving eq, show]
+
+  let init () =
+    match Yaml.of_string_exn [%blob "../uap-core/regexes.yaml"] with
+    | `O assoc ->
+        List.Assoc.find_exn assoc ~equal:String.equal "user_agent_parsers"
+        |> Parser.of_yaml
+    | _ -> failwith "invalid yaml"
+
+  let parse t ua =
+    let result = Parser.apply t ua in
+    {
+      family = Parser.Result.get result ("family_replacement", 1) |> Option.value ~default:"Other";
+      major = Parser.Result.get result ("v1_replacement", 2);
+      minor = Parser.Result.get result ("v2_replacement", 3);
+      patch = Parser.Result.get result ("v3_replacement", 4);
+    }
+end
+
+module OSParser = struct
+  type t = Parser.t
+
+  type result = {
+    family: string;
+    major: string option;
+    minor: string option;
+    patch: string option;
+    patch_minor: string option;
+  } [@@deriving eq, show]
+
+  let init () =
+    match Yaml.of_string_exn [%blob "../uap-core/regexes.yaml"] with
+    | `O assoc ->
+        List.Assoc.find_exn assoc ~equal:String.equal "os_parsers"
+        |> Parser.of_yaml
+    | _ -> failwith "invalid yaml"
+
+  let parse t ua =
+    let result = Parser.apply t ua in
+    {
+      family = Parser.Result.get result ("os_replacement", 1) |> Option.value ~default:"Other";
+      major = Parser.Result.get result ("os_v1_replacement", 2);
+      minor = Parser.Result.get result ("os_v2_replacement", 3);
+      patch = Parser.Result.get result ("os_v3_replacement", 4);
+      patch_minor = Parser.Result.get result ("os_v4_replacement", 5);
+    }
+end
+
+module DeviceParser = struct
+  type t = Parser.t
+
+  type result = {
+    family: string;
+    brand: string option;
+    model: string option;
+  } [@@deriving eq, show]
+
+  let init () =
+    match Yaml.of_string_exn [%blob "../uap-core/regexes.yaml"] with
+    | `O assoc ->
+        List.Assoc.find_exn assoc ~equal:String.equal "device_parsers"
+        |> Parser.of_yaml
+    | _ -> failwith "invalid yaml"
+
+  let parse t ua =
+    let result = Parser.apply t ua in
+    {
+      family = Parser.Result.get result ("device_replacement", 1) |> Option.value ~default:"Other";
+      brand = Parser.Result.get result ("brand_replacement", 2);
+      (* Not a bug, model replacement is supposed to be 1, see JS ref implementation *)
+      model = Parser.Result.get result ("model_replacement", 1);
+    }
+end
